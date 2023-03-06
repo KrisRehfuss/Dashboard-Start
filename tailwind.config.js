9ca214a4a2f61c70c7c75ae5aea4b47780792958
KrisRehfuss@gmail.com <KrisRehfuss@gmail.com>
2023-03-06 15:06:17 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    screens: {
+      sm: "380px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontSize: {
+        bio: "13px",
+        QR: "15px",
+      },
+      colors: {
+        coal: "#191919",
+        pri: "#8e949e",
+        whyte: "hsl(0, 0%, 100%)",
+        Lgray: "hsl(212, 45%, 89%)",
+      },
+      fontFamily: {
+        inter: ["inter", "serif"],
+        mont: ["Montserrat", "sans-serif"],
+        pop: ["poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
